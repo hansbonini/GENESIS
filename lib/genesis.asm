024d768089f34acbac36ce26850b9d36750e9033
Hans Bonini <hans@ysolutions.com.br>
2021-01-02 19:39:00 -0300

    Basic library

---
+++ lib/genesis.asm
@@ -0,0 +1,149 @@
+constant MEGACD($400000)
+constant MEGA32X($800000)
+constant MEGA32X_FB($840000)
+constant MEGA32X_FB_OW($860000)
+constant MEGA32X_ROM($880000)
+constant MEGA32X_ROM_BS($900000)
+constant Z80_RAM($A00000)
+constant VERSION($A10001)
+constant JOYSTICK1_DATA($A10002)
+constant JOYSTICK2_DATA($A10004)
+constant EXPANSION_DATA($A10006)
+constant JOYSTICK1_CTRL($A10008)
+constant JOYSTICK2_CTRL($A1000A)
+constant EXPANSION_CTRL($A1000C)
+constant Z80_BUS($A111101)
+constant Z80_RESET($A11201)
+constant TMSS($A14000)
+constant VDP_DATA($C00000)
+constant VDP_DATA2($C00002)
+constant VDP_CTRL($C00004)
+constant VDP_CTRL2($C00006)
+constant HV_COUNTER($C00008)
+constant HV_COUNTER2($C0000A)
+constant HV_COUNTER3($C0000C)
+constant HV_COUNTER4($C0000E)
+constant PSG($C00011)
+constant PSG2($C00013)
+constant PSG3($C00015)
+constant PSG4($C00017)
+constant DEBUG($C0001C)
+constant DEBUG2($C0001E)
+constant RAM($FF0000)
+constant HBLANK_COUNTER($FF0000)
+constant VBLANK_COUNTER(HBLANK_COUNTER+4)
+
+constant VDP_CTRL_VRAM_READ($00000000)
+constant VDP_CTRL_VRAM_WRITE($40000000)
+constant VDP_CTRL_CRAM_READ($00000020)
+constant VDP_CTRL_CRAM_WRITE($C0000000)
+constant VDP_CTRL_VSRAM_READ($00000010)
+constant VDP_CTRL_VSRAM_WRITE($40000010)
+
+constant VDP_NUM_REGISTER($18)
+constant VDP_REGS($8000)
+constant VDP_REG_HINT($8100)
+constant VDP_REG_VINT($8200)
+constant VDP_REG_SCROLL_A($8300)
+constant VDP_REG_WINDOW($8400)
+constant VDP_REG_SCROLL_B($8500)
+constant VDP_REG_SPRITES($8600)
+constant VDP_REG_BGCOLOR($8800)
+constant VDP_REG_HINT_FREQ($8A00)
+constant VDP_REG_EXTINTERRUPTS_HVFULLSCREEN($8B00)
+constant VDP_REG_SHADOWS_INTERLACE_H40($8C00)
+constant VDP_REG_HSCROLL($8D00)
+constant VDP_REG_AUTOINCREMENT($8F00)
+constant VDP_REG_HVSCROLL($9000)
+constant VDP_REG_WINDOW_X($9100)
+constant VDP_REG_WINDOW_Y($9200)
+constant VDP_REG_DMALEN_LO($9300)
+constant VDP_REG_DMALEN_HI($9400)
+constant VDP_REG_DMASRC_LO($9500)
+constant VDP_REG_DMASRC_MI($9600)
+constant VDP_REG_DMASRC_HI($9700)
+constant VDP_WRITE_PALETTES($C0000000)
+
+constant JOYSTICK_LATCH($40)
+
+constant STACK_TOP($00FFE000)
+constant RAM_SIZE_B($0000FFFF)
+constant RAM_SIZE_W(RAM_SIZE_B/2)
+constant RAM_SIZE_L(RAM_SIZE_B/4)
+
+macro clearRegisters() {
+    move.l  #RAM,a0      		                    //; Move address of first byte of ram (contains zero,RAM has been cleared) to a0
+	movem.l (a0),#($FFFD)      	                    //; Multiple move zero to all registers (except sp)
+	move.l  #$00000000,a0      	                    //; Clear a0
+}
+macro SaveAllRegistersToSP() {
+    movem.l #$FFFF,-(a7)                            //; Save all registers a0-a7/d0-d7 into stack (sp) 
+}
+macro LoadAllRegistersFromSP() {
+    movem.l (a7)+,#$FFFF                            //; Load all registers a0-a7/d0-d7 into stack (sp)
+}
+
+macro init(MAIN) {
+    bra         +
+-
+    jmp         {MAIN}
++
+    tst.w       (JOYSTICK1_CTRL).l                  //; Test Joystick 1
+	bne -                                           //; Branch if Not Equal (to zero) - to Main
+	tst.w       (EXPANSION_CTRL).l                  //; Test Expansion Port
+	bne -                                           //; Branch if Not Equal (to zero) - to Main
+}
+
+macro initTMSS() {  
+	move.b      (VERSION).l,d0                      //; Move Megadrive hardware version to d0
+	andi.b      #$0F,d0                             //; The version is stored in last four bits, so mask it with 0F
+	beq         +                                   //; If version is equal to 0, skip TMSS signature
+	move.l      #"SEGA",(TMSS).l                    //; Move the string "SEGA" to $A14000 (TMSS)
+    +
+}
+
+macro clearRAM(START, LENGTH) {
+    move.l      #{START},a0                         //; Set start address
+    move.l      #({LENGTH})/4,d1                    //; Set length
+-
+    move.l      #$00000000,(a0)+                    //; Move 4 bytes to address then increment
+    dbra        d1,-                                //; Loop until equal length
+}
+
+macro loadRAM(SRC, LENGTH, DEST) {
+    SaveAllRegistersToSP()
+
+    movea.l #RAM,a1
+    movea.l #({SRC}),a0
+    clr.l   d0
+    move.l  #(({LENGTH}/32)-3),d0
+-;
+    move.l  (a0)+,(a1)+                             //; TILE LINE #1
+    move.l  (a0)+,(a1)+                             //; TILE LINE #2
+    move.l  (a0)+,(a1)+                             //; TILE LINE #3
+    move.l  (a0)+,(a1)+                             //; TILE LINE #4
+    move.l  (a0)+,(a1)+                             //; TILE LINE #5
+    move.l  (a0)+,(a1)+                             //; TILE LINE #6
+    move.l  (a0)+,(a1)+                             //; TILE LINE #7
+    move.l  (a0)+,(a1)+                             //; TILE LINE #8
+    dbf     d0,-
+
+    LoadAllRegistersFromSP()
+}
+
+macro disableInterrupts() {
+    move        #$2700,sr                           //; Disable all Interrupts
+}
+
+macro enableInterrupts() {
+    move.w      #$2000,sr                           //; Enable all Interrupts
+}
+
+macro setInterruptLevel(LEVEL) {
+	move.w      sr,d0
+    if ({LEVEL} == 3) {
+        andi.w #$F8FF,d0	                        // ; INT level 3 (all interrupts)
+    }
+	move.w      d0,sr
+    clr.l       d0			                        // ; Clear d0
+}
---
+++ lib/genesis_gfx.asm
@@ -0,0 +1,122 @@
+macro waitVBlank() {
+-
+	move.w 	VDP_CTRL,d0 		                    // ; copy VDP status to d0
+	btst   	#3,d0     			                    // ; vblank state in bit 3
+	beq  	- 	                                    // ; wait for vblank to complete
+}
+
+macro setVDPRegisters(SRC) {
+	move.l      #{SRC},a0   		  	            // ; Load address of register table into a0
+	move.l      #(VDP_NUM_REGISTER-1),d0 	        // ; 24 registers to write (-1 for loop counter)
+	move.l      #VDP_WRITE_REGISTER,d1    	        // ; 'Set register 0' command
+-
+	move.b      (a0)+,d1          			        // ; Move register value to lower byte of d1
+	move.w      d1,VDP_CTRL     			        // ; Write command and value to VDP control port
+	add.w       #$0100,d1          			        // ; Increment register #
+	dbra        d0,-
+}
+
+macro clearCRAM() {
+	move.l      #VDP_WRITE_PALETTES,VDP_CTRL        // ; Write to palette memory
+	move.l      #$3F,d1                             // ; CRAM size (in words)
+-	
+	move.w      #0,VDP_DATA                         // ; Write 0 (autoincrement is 2)
+	dbra        d1,-
+}
+macro clearVRAM() {
+    SaveAllRegistersToSP()
+    
+    // ; Set autoincrement to 1 byte
+	move.w      #(VDP_REG_AUTOINCREMENT+$0001),(VDP_CTRL).l
+    // ; Set bytes to DMA fill (lo) (reg 19)
+	move.w      #(VDP_REG_DMALEN_LO+$00FF)(VDP_CTRL).l
+    // ; Set bytes to fill (hi) (reg 20)
+	move.w      #(VDP_REG_DMALEN_HI+$00FF),(VDP_CTRL).l
+    // ; Set DMA to Fill (reg 23,bits 0-1)
+	move.w      #(VDP_REG_DMASRC_HI+$0080)(VDP_CTRL).l
+ 			
+	move.l      #$40000080,VDP_CTRL 		        // ; Set destination address
+	move.w      #$0,VDP_DATA           		        // ; Value to write
+
+-                    
+	move.w 	    VDP_CTRL,d1          		        // ; Read VDP status reg
+	btst    	#1,d1                 		        // ; Check if DMA finished
+	bne  	    -
+}
+macro clearVSRAM() {
+
+    // ; Set bytes to DMA fill (lo) (reg 19)
+	move.w      #(VDP_REG_DMALEN_LO+$0050),(VDP_CTRL).l
+    // ; Set bytes to fill (hi) (reg 20)
+	move.w      #VDP_REG_DMALEN_HI,(VDP_CTRL).l
+    // ; Set DMA to Fill (reg 23,bits 0-1)
+	move.w      #(VDP_REG_DMASRC_HI+$0080),(VDP_CTRL).l
+
+	move.l      #$40000090,VDP_CTRL 			    // ; Set destination address
+	move.w      #$0,VDP_DATA           		        // ; Value to write
+-                    
+	move.w 	    VDP_CTRL,d1          		        // ; Read VDP status reg
+	btst   	    #1,d1                 		        // ; Check if DMA finished
+	bne  	    -
+
+    // ; Set autoincrement to 1 byte
+	move.w      #(VDP_REG_AUTOINCREMENT+$0002),(VDP_CTRL).l
+    // ; Set H interrupt timing
+	move.w      #(VDP_REG_HINT_FREQ+$00DF),(VDP_CTRL).l  			
+}
+
+
+macro loadVRAM(SRC, LENGTH, DEST) {
+    SaveAllRegistersToSP()
+
+    move.l  #($VDP_CTRL_VRAM_WRITE+(({DEST}&$3FFF)<<16)+(({DEST}&$C000)>>14)),(VDP_CTRL).l
+    movea.l #({SRC}),a0
+    clr.l   d0
+    move.l  #(({LENGTH}/32)-3),d0
+-;
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #1
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #2
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #3
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #4
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #5
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #6
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #7
+    move.l  (a0)+,(VDP_DATA).l                      //; TILE LINE #8
+    dbf     d0,-
+
+    LoadAllRegistersFromSP()
+}
+
+
+macro dmaLoadVRAM(SRC, LENGTH, DEST) {
+    SaveAllRegistersToSP()
+    move.w #(VDP_REG_HINT+$0074),(VDP_CTRL).l
+    move.w #(VDP_REG_AUTOINCREMENT+$0002),(VDP_CTRL).l
+    move.w #(VDP_REG_DMALEN_LO+((({LENGTH})>>1)&$FF)),(VDP_CTRL).l
+    move.w #(VDP_REG_DMALEN_HI+(((({LENGTH})>>1)&$FF00)>>8)),(VDP_CTRL).l
+    move.w #(VDP_REG_DMASRC_LO+(({SRC}>>1)&$FF)),(VDP_CTRL).l
+    move.w #(VDP_REG_DMASRC_MI+((({SRC}>>1)&$FF00)>>8)),(VDP_CTRL).l
+    move.w #(VDP_REG_DMASRC_HI+((({SRC}>>1)&$7F0000)>>16)),(VDP_CTRL).l
+    move.l #$40000080+(({DEST}&$3FFF)<<16)+(({DEST}&$C000)>>14),(VDP_CTRL).l
+-                    
+	move.w 	VDP_CTRL,d1          		            // ; Read VDP status reg
+	btst   	#1,d1                 		            // ; Check if DMA finished
+	bne  	-
+    move.w  #(VDP_REG_HINT+$0064),(VDP_CTRL).l
+    LoadAllRegistersFromSP()
+}
+
+
+macro dmaFillVRAM(DEST, LENGTH) {
+    SaveAllRegistersToSP()
+
+    move.w #(VDP_REG_HINT+$0074),(VDP_CTRL).l
+    move.w #(VDP_REG_AUTOINCREMENT+$0001),(VDP_CTRL).l
+    move.w #(VDP_REG_DMALEN_LO+(({LENGTH}>>1)&$FF)),($c00004).l
+    move.w #(VDP_REG_DMALEN_HI+((({LENGTH}>>1)&$FF00)>>8)),($c00004).l
+    move.w #(VDP_REG_DMASRC_HI+$0080),(VDP_DATA).l
+    move.l #$40000080+(({DEST}&$3FFF)<<16)+(({DEST}&$C000)>>14),(VDP_DATA).l
+    move.w #(VDP_REG_HINT+$0064),(VDP_CTRL).l
+
+    LoadAllRegistersFromSP()
+}
